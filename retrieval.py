e57afec9e48c9cbc72e203981fff6ac182c2634c
Sohini Roy <sohiniroy@Sohinis-MacBook-Pro.local>
2025-04-25 00:26:24 -0400

    Country question

--- retrieval.py
+++ retrieval.py
@@ -112,16 +112,4 @@ def answer_question(question, top_k=5):
     print("\n[Answer]")
     print(result)
 
-answer_question("Summarize disasters occurring in Africa in 2015.")
-
-# if __name__ == "__main__":
-#     print("🔍 Welcome to the Disaster QA System for Africa (2015–2025)")
-#     print("Type your question about disasters")
-#     print("Type 'exit' or 'quit' to end the session.\n")
-
-#     while True:
-#         user_input = input("Ask a question: ").strip()
-#         if user_input.lower() in {"exit", "quit"}:
-#             print("👋 Exiting. Stay safe!")
-#             break
-#         answer_question(user_input)
+answer_question("What are some disasters that have occurred in Nigeria over the past two years?")
